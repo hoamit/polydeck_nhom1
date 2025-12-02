28ff40658a418a24cdd8dc7524c5daa4cfd1f3f4
hoanguyen00t <hoahaha006dx@gmail.com>
2025-12-02 09:07:40 +0700

    model

--- PolyDeck_server/models/BaiQuiz.js
+++ PolyDeck_server/models/BaiQuiz.js
@@ -1,41 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // --- Định nghĩa cấu trúc cho một câu trả lời ---
-const answerSchema = new Schema({
+const answerSchema = new Schema(
+  {
     answerText: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    isCorrect: { // Trường này để xác định đáp án đúng
-        type: Boolean,
-        required: true,
-        default: false
-    }
-}, { _id: false });
+    isCorrect: {
+      // Trường này để xác định đáp án đúng
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { _id: false }
+);
 
 // --- Định nghĩa cấu trúc cho một câu hỏi ---
-const questionSchema = new Schema({
+const questionSchema = new Schema(
+  {
     questionText: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    answers: [answerSchema] // Một câu hỏi có một mảng các câu trả lời
-}, { _id: false });
+    answers: [answerSchema], // Một câu hỏi có một mảng các câu trả lời
+  },
+  { _id: false }
+);
 
 // --- Định nghĩa cấu- trúc chính cho Bài Quiz ---
-const baiQuizSchema = new Schema({
-  ma_chu_de: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'ChuDe', // Tham chiếu đến model 'ChuDe'
-    required: true 
+const baiQuizSchema = new Schema(
+  {
+    ma_chu_de: {
+      type: Schema.Types.ObjectId,
+      ref: "ChuDe", // Tham chiếu đến model 'ChuDe'
+      required: true,
+    },
+    questions: [questionSchema], // <<< FIX: Bổ sung mảng câu hỏi
   },
-  questions: [questionSchema] // <<< FIX: Bổ sung mảng câu hỏi
-}, {
-  timestamps: true,
-  collection: 'bai_quiz'
-});
+  {
+    timestamps: true,
+    collection: "bai_quiz",
+  }
+);
 
 baiQuizSchema.index({ ma_chu_de: 1 });
 
-module.exports = mongoose.model('BaiQuiz', baiQuizSchema);
+module.exports = mongoose.model("BaiQuiz", baiQuizSchema);
